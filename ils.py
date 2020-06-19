13dd0eb881e3415acf1da294788f892145904a60
CrazyR99 <63822097+CrazyR99@users.noreply.github.com>
2020-06-20 00:36:16 +0200

    fin de init(), solution valide changement du critère alpha et compteur dans Uj et Vk

--- ils.py
+++ ils.py
@@ -3,45 +3,50 @@ from read_excel import *
 Notation utilisée par la suite
 L_ce  #liste customer/end office
 L_ed  #liste end-office/digital hub
-L_dd  #liste digital hub/digital hub
+L_dd  #liste digital hub/digital hub le numéro en position k indique à quel hub est relié le hub k
 """
 
 def init():
-	Lce=[[0 for k in range(M)]for l in range(C)]
-	Led = [[0 for k in range(N)] for l in range(M)]
-	Ldd = [[0 for k in range(N)] for l in range(N)]
+	Lce=[0 for k in range(C)]
+	Led = [0 for k in range(M)]
+	Ldd = [0 for k in range(N)]
 
 	capa_end = 0
 	capa_digi = 0
-	compteur = 0
+	compteur = 1
 
-	for customer in C:
+	for customer in range(C):
 		capa_end += 1
-		if capa_end <= Uj[compteur]:
-			Lce[customer][compteur] = 1
+		if capa_end <= Uj[compteur-1]:
+			Lce[customer] = compteur
 		else:
-			capa_end = 1
+			capa_end = 1 #sous-entend que chaque end office a une capa de 1 minimum
 			compteur += 1
 			if compteur > M:
 				break
 			else:
-				Lce[customer][compteur] = 1
+				Lce[customer] = compteur
 
-	compteur = 0
+	compteur = 1
 
-	for end in M:
+	for end in range(M):
 		capa_digi += 1
-		if capa_digi <= Uj[compteur]:
-			Lce[customer][compteur] = 1
+		if capa_digi <= Uj[compteur-1]:
+			Led[end] = compteur
 		else:
+			capa_digi = 1 #sous-entend que chaque hub a une capa de 1 minimum
 			compteur += 1
-			if compteur > M:
+			if compteur > N:
 				break
 			else:
-				Lce[customer][compteur] = 1
+				Led[end] = compteur
 
+	for k in range(compteur-1):
+		Ldd[k]=k+2
+	Ldd[compteur-1]=1
 
-	return None
+	return Lce,Led,Ldd
+
 
 def verif(L_ce,L_ed,L_dd): #verifies it is a solution
 	
@@ -50,7 +55,7 @@ def verif(L_ce,L_ed,L_dd): #verifies it is a solution
 	for i in range(len(L_ce)):
 		if L_ce[i] == 0:
 			customers_pas_servis += 1
-	if customers_pas_servis/len(L_ce) > alpha:
+	if 1-(customers_pas_servis/len(L_ce)) < alpha:
 		return False
 
 	#Verification des capacites
@@ -59,7 +64,7 @@ def verif(L_ce,L_ed,L_dd): #verifies it is a solution
 		for j in range(len(L_ce)):
 			if L_ce[j] == i:
 				utilisateurs += 1
-		if utilisateurs > Uj[i]:
+		if utilisateurs > Uj[i-1]:
 			return False
 
 	for i in range(1,len(L_dd)+1): #Les capacites des digital hubs
@@ -67,7 +72,7 @@ def verif(L_ce,L_ed,L_dd): #verifies it is a solution
 		for j in range(len(L_ed)):
 			if L_ed[j] == 1:
 				utilisateurs += 1
-		if utilisateurs > Vk[i]:
+		if utilisateurs > Vk[i-1]:
 			return False
 
 	#Verifie si plus de 3 digital hubs sont selectionnes
@@ -100,7 +105,9 @@ def score(L_ce,L_ed,L_dd): #Calculates the objective function value for a given 
 def neighbor():
 	return None
 
-def swap():
+def swap(Lce,Led,Ldd):
+
+
 	return None
 
 def intensification():
@@ -109,3 +116,6 @@ def intensification():
 def perturbation():
 	return None
 
+LCE,LED,LDD=init()
+
+print(verif(LCE,LED,LDD))
